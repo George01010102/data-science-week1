10f4036c07a2e1bd2b49827c9a1795c6dabe3340
Your Name <your.email@uea.ac.uk>
2026-02-09 13:06:02 +0000

    Added script to check for duplicates in pengiun data

---
+++ scripts/Duplicates.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+# check for whole duplicate 
+# rows in the data
+penguins_clean_names |> 
+  filter(duplicated(across(everything())))
+sum() 
+
+penguins_demo <- penguins_clean_names |> 
+  slice(1:50) |> 
+  bind_rows(slice(penguins_clean_names, c(1,5,10,15,30)))
+
+penguins_demo |> 
+  filter(duplicated(across(everything())))
+sum()
+
+penguins_clean_names |> 
+  summarise(
+    n = n(),
+    n_distinct(individual_id)
+  )
